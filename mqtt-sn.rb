19db3dcfc083914f0ffb5f42ee2d0dec41578094
Ari Siitonen <jalopuuverstas@gmail.com>
2014-11-16 14:33:39 +0200

    cleanup

--- mqtt-sn.rb
+++ mqtt-sn.rb
@@ -3,6 +3,7 @@
 
 require "pp"
 require 'socket'
+require 'json'
 
 $s = UDPSocket.new
 
@@ -27,7 +28,7 @@ QOS2_FLAG  =0x40
 QOS1_FLAG  =0x20
 QOS0_FLAG  =0x00
 
-$msg_id=0x1234
+$msg_id=1
 $iq = Queue.new
 
 def send_packet m
@@ -38,17 +39,19 @@ def send_packet m
     len+=1
   end
   msg[0]=len.chr
-  print ">> "
-  msg.each_byte do |ch|
-    printf "%02X ",ch
+  if false
+    print ">> "
+    msg.each_byte do |ch|
+      printf "%02X ",ch
+    end
+    puts ""
   end
-  puts ""
   $s.send(msg, 0, '20.20.20.21', 1883)
 end
 
 def send type,hash={},&block
   puts ""
-  pp type,hash
+  puts "#{type},#{hash.to_json}"
   case type
   when :connect 
     p=[CONNECT_TYPE,CLEAN_FLAG,0x01,0,30]
@@ -56,22 +59,27 @@ def send type,hash={},&block
       p<<b
     end
   when :register 
-    p=[REGISTER_TYPE,0,0,hash[:msg_id] >>8 ,hash[:msg_id] & 0xff]
+    raise "Need :topic to Publish!" if not hash[:topic]
+    p=[REGISTER_TYPE,0,0,$msg_id >>8 ,$msg_id & 0xff]
     hash[:topic].each_byte do |b|
       p<<b
     end
-  when :publish 
-    p=[PUBLISH_TYPE,0x20*(hash[:qos]||0),hash[:topic_id] >>8 ,hash[:topic_id] & 0xff,hash[:msg_id] >>8 ,hash[:msg_id] & 0xff]
+    $msg_id+=1
+  when :publish
+    raise "Need :topic_id to Publish!" if not hash[:topic_id]
+    p=[PUBLISH_TYPE,0x20*(hash[:qos]||0),hash[:topic_id] >>8 ,hash[:topic_id] & 0xff,$msg_id >>8 ,$msg_id & 0xff]
     hash[:msg].each_byte do |b|
       p<<b
     end
+    $msg_id+=1
   when :pubrel 
-    p=[PUBREL_TYPE,hash[:msg_id] >>8 ,hash[:msg_id] & 0xff]
+    p=[PUBREL_TYPE,$msg_id >>8 ,$msg_id & 0xff]
+    $msg_id+=1
   when :ping
     p=[PINGREQ_TYPE]
     reply_type=:pong
   else
-    puts "Strange send?? #{type}"
+    puts "Error: Strange send?? #{type}"
     return nil
   end
   $iq.clear
@@ -81,17 +89,14 @@ def send type,hash={},&block
   status=:timeout
   m={}
   if hash[:expect] #wait timeout time for reply_type to arrive..
-    puts "waiting for #{hash[:expect]}, timeout: #{timeout}"
+    #puts "waiting for #{hash[:expect]}, timeout: #{timeout}"
     while Time.now.to_i<stime+timeout
       if not $iq.empty?
         m=$iq.pop
-        puts "from queue:"
-        pp m
+        #pp m
         if Array(hash[:expect]).include? m[:type]
           status=:ok
           break
-        else
-          puts "wrong type!"
         end
       end
       sleep 0.1
@@ -108,11 +113,7 @@ t=Thread.new do
   while true do
     begin # emulate blocking recvfrom
       r,stuff=$s.recvfrom_nonblock(200)
-      print "<< "
-      r.each_byte do |b|
-        printf "%02X ",b
-      end
-      puts ""
+     
 
       m=nil
       len=r[0].ord
@@ -147,8 +148,13 @@ t=Thread.new do
         m={type: :pong, status: :ok}
       else
         m={type: :unknown, type_byte: type_byte }
+        print "<< "
+        r.each_byte do |b|
+          printf "%02X ",b
+        end
+        puts ""
       end
-      pp m
+      puts "got: #{m.to_json}"
       $iq<<m if m
     rescue IO::WaitReadable
       IO.select([$s])
@@ -158,26 +164,28 @@ t=Thread.new do
 end
 
 send :connect,id: "tadaa", expect: :connect_ack do |s,m|
-  puts "got connection! status=#{s}, message=#{m}"
+  puts "got connection! status=#{s}, message=#{m.to_json}"
 end
-send :register,topic:"top",msg_id:$msg_id, expect: :register_ack do |s,m|
-  puts "got topic! status=#{s}, message=#{m}"
+send :register,topic:"top", expect: :register_ack do |s,m|
+  puts "got topic! status=#{s}, message=#{m.to_json}"
 end
 
-$msg_id+=1
-send :publish,msg:"tadaa",topic_id: 1, msg_id:$msg_id, qos: 2, expect: [:publish_ack,:pubrec] do |s,m|
-  puts "got published! status=#{s}, message=#{m}"
+send :publish,msg:"tadaa",topic_id: 1, qos: 2, expect: [:publish_ack,:pubrec] do |s,m|
+  puts "got published! status=#{s}, message=#{m.to_json}"
   if s==:ok
     if m[:type]==:pubrec
       send :pubrel,msg_id: m[:msg_id], expect: :pubcomp do |s,m|
-        puts "got handshaken! status=#{s}, message=#{m}"
+        puts "got handshaken! status=#{s}, message=#{m.to_json}"
       end
     end
   end
 end
 
-send :ping, timeout: 3, expect: :pong do |status,message|
-  puts "got bong! status=#{status}, message=#{message}"
+while true do
+  send :ping, timeout: 3, expect: :pong do |status,message|
+    puts "got bong! status=#{status}, message=#{message.to_json}"
+  end
+  sleep 5
 end
 
 t.join
