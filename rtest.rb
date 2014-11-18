d59382f84b049e8a23ce5c5bc03ab775e18f4c2a
Ari Siitonen <jalopuuverstas@gmail.com>
2014-11-18 20:59:37 +0200

    uri scheme and roaming...

--- rtest.rb
+++
@@ -1,6 +0,0 @@
-require 'socket'
-server = UDPSocket.open
-server.bind('0.0.0.0', 1882)
-while true do
-  p server.recvfrom(10)
-end
--- stest.rb
+++
@@ -1,4 +0,0 @@
-require 'socket'
-socket = UDPSocket.open
-socket.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
-socket.send("sample", 0, '255.255.255.255', 4321)
